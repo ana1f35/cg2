0940229a05aeb83e41011dd7f8c6d7360b59fd76
ana1f35 <anafsilva2004@gmail.com>
2025-01-05 19:44:50 +0000

    faltou um ficheiro

--- headers/audio.h
+++ headers/audio.h
@@ -6,7 +6,6 @@
 #include <string>
 #include <thread>
 
-void playAudio(ALuint buffer);
 bool loadAudio(const std::string& fileName, ALuint& buffer);
 int inicializarSound(ALCdevice*& device, ALCcontext*& context, 
     ALuint& buffer, ALuint& buffer2, ALuint& buffer3, ALuint& buffer4, ALuint& source, ALuint& source2, ALuint& source3, ALuint& source4);
